711f8039aacdaa4724bbea1c39e65808503187a0
onomeh <onwuanibeonome@gmail.com>
2022-03-30 19:46:28 +0000

    Fi errors

--- webpack.config.js
+++ webpack.config.js
@@ -27,19 +27,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
+      }, 
     ],
   },
 };
